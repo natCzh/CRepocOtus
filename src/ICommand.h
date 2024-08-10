52f5589b7de9b784e9db7a87efd763935291bb9f
Natalia Zhukova <njukova@stc-spb.ru>
2024-08-10 17:49:49 +0300

    [+] добавлены первые файлы

---
+++ src/ICommand.h
@@ -0,0 +1,10 @@
+﻿#ifndef _I_COMMAND_H_
+#define _I_COMMAND_H_
+
+class ICommand
+{
+public:
+	virtual int Execute() = 0;
+};
+
+#endif _I_COMMAND_H_
---
+++ src/QueueCommand.h
@@ -0,0 +1,7 @@
+#ifndef _QUEUE_COMMAND_H
+#define _QUEUE_COMMAND_H
+
+
+
+
+#endif _QUEUE_COMMAND_H
