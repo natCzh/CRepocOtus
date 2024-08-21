5751838f168a0781285607ef772d48192d8ab374
Natalia Zhukova <njukova@stc-spb.ru>
2024-08-21 11:09:32 +0300

    [+] добавлена команда BurnFuelComamnd и тесты к ней, добавлена SimpleMacroCommand

--- src/Command.h
+++ src/Command.h
@@ -2,6 +2,7 @@
 #define _COMAAND_H
 
 #include <memory>
+#include <list>
 
 #include "ICommand.h"
 #include "Fuel.h"
@@ -30,6 +31,48 @@ public:
 public:
 	std::shared_ptr<Fuel>		fuelCur;		// сущность с топливом
 	int							countDistance;	// дистанция на которую надо проверить топливо и сместить потом
+};
+
+class BurnFuelComamnd : public ICommand
+{
+public:
+	BurnFuelComamnd(std::shared_ptr<Fuel> &fuelValue, int countDistValue)
+		: fuelCur(fuelValue)
+		, countDistance(countDistValue)
+	{}
+
+	void Execute() override
+	{
+		int res = fuelCur->getAmountFuel() - countDistance * fuelCur->getConsumptionUnit();
+		fuelCur->setAmountFuel(res);
+	}
+
+	std::string GetType() override
+	{
+		return "BurnFuelComamnd";
+	}
+
+public:
+	std::shared_ptr<Fuel>		fuelCur;		// сущность с топливом
+	int							countDistance;	// дистанция на которую надо проверить топливо и сместить потом
+};
+
+class SimpleMacroCommand : public ICommand
+{
+public:
+	SimpleMacroCommand(const std::shared_ptr<std::vector<ICommand> > cmds_value)
+		: cmds(cmds_value)
+	{}
+
+	void Execute()
+	{
+		unsigned int sizeData = cmds->size();
+		for (unsigned int it = 0; it < sizeData; it++)
+			cmds->at(it).Execute();
+	}
+
+private:
+	std::shared_ptr<std::vector<ICommand> > cmds;
 };
 
 #endif /* _COMAAND_H */
--- tests/TestFuelCommand.cpp
+++ tests/TestFuelCommand.cpp
@@ -10,21 +10,31 @@ using namespace testing;
 TEST(TestFuelCommand, testCheckFuelCommand)
 {
 	// кол-во топлива хватает
-	Fuel fuelValue;
-	fuelValue.setAmountFuel(6);
-	fuelValue.setConsumptionUnit(1);
+	std::shared_ptr<Fuel> fuelValue(std::make_shared<Fuel>());
+	fuelValue->setAmountFuel(6);
+	fuelValue->setConsumptionUnit(1);
 	int countDistValue = 4;
-	CheckFuelComamnd checkFuel(std::make_shared<Fuel>(fuelValue), countDistValue);
+	CheckFuelComamnd checkFuel(fuelValue, countDistValue);
 	EXPECT_NO_THROW(checkFuel.Execute());
 
 	// кол-во топлива ровно
 	countDistValue = 6;
-	CheckFuelComamnd checkFuel2(std::make_shared<Fuel>(fuelValue), countDistValue);
+	CheckFuelComamnd checkFuel2(fuelValue, countDistValue);
 	EXPECT_NO_THROW(checkFuel2.Execute());
 
 	// кол-во топлива не хватает
 	countDistValue = 8;
-	CheckFuelComamnd checkFuel3(std::make_shared<Fuel>(fuelValue), countDistValue);
-	EXPECT_THROW(checkFuel3.Execute(), std::exception);
+	CheckFuelComamnd checkFuel3(fuelValue, countDistValue);
 	EXPECT_THROW(checkFuel3.Execute(), CommandException);
+}
+
+TEST(TestFuelCommand, testBurnFuelCommand)
+{
+	std::shared_ptr<Fuel> fuelValue(std::make_shared<Fuel>());
+	fuelValue->setAmountFuel(6);
+	fuelValue->setConsumptionUnit(1);
+	int countDistValue = 4;
+	BurnFuelComamnd burnFuel(fuelValue, countDistValue);
+	burnFuel.Execute();
+	EXPECT_EQ(fuelValue->getAmountFuel(), 2);
 }
