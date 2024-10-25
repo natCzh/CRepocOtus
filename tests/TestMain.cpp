49a71892c5dcf77e372b5cc9163f84e38b8726d1
Natalia Zhukova <njukova@stc-spb.ru>
2024-10-25 15:39:11 +0300

    [*] intermediate commit

--- tests/TestMain.cpp
+++ tests/TestMain.cpp
@@ -1,1 +1,24 @@
-﻿
+﻿#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "src/Common/ThreadDeque.h"
+#include "src/Common/IThreadDeque.h"
+#include "src/Command/ICommand.h"
+
+
+// пример пользования очередью
+TEST(TEST1, test)
+{
+	ThreadDeque<int> queue;
+	//std::shared_ptr<IThreadDeque<int> > queue_ptr = std::make_shared<ThreadDeque<int> >(queue);
+	int v = 1;
+	queue.push(v);
+	int s;
+	auto res = queue.pop(1);
+	res = queue.pop(0);
+
+	// queue_ptr->tryPop(v, 1);
+
+	int sdfs = 0;
+}
+
