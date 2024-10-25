38f76da150920e8e5f2a25d4deba9e363b3c6432
Natalia Zhukova <njukova@stc-spb.ru>
2024-10-25 13:33:00 +0300

    [*] intermediate commit deletable

--- tests/TestIoCCommand.cpp
+++ tests/TestIoCCommand.cpp
@@ -6,6 +6,8 @@
 #include "src/Command/MoveCommand.h"
 #include "src/Command/ICommand.h"
 
+#include <typeinfo>
+
 class MoveCommandMock : public ICommand
 {
 public:
@@ -18,11 +20,54 @@ public:
 	MOCK_METHOD0(Execute, void());
 };
 
+#include <functional> 
+	class A
+	{
+	public:
+
+					// вызов стратегии
+		template<typename T, typename ... ARG>
+		T resolve(ARG ... arg)
+		{
+
+
+
+			std::cout << "resolve 1 " << std::typeid(T).name() << std::endl;
+		}
+
+
+
+
+			// вызов стратегии
+		template<typename T>
+		T resolve(std::string &type)
+		{
+			int e = 0;
+//			auto cx = std::typeid(e);
+			//std::cout << "resolve 1 " << std::typeid(T).name() << std::endl;
+			return (T)e;
+		}
+
+		// регистрация
+		template<typename T>
+		T resolve(std::string &type, std::string &structType, std::function < T(void)>)
+		{
+			std::cout << "resolve 2 " << std::typeid(T).name() << ", " << structType << std::endl;
+		}
+	};
+
+
+TEST(TestIoCCommand, registerCommand1)
+{
+	A a;
+	std::string str1 = "1int";
+	int res = a.resolve<int>(str1);
+
+}
+
 
 TEST(TestIoCCommand, registerCommand)
 {
-
-
 	IoC ioc;
 
 	// добавляем новый скоуп
@@ -34,13 +79,28 @@ TEST(TestIoCCommand, registerCommand)
 	MoveCommandMock moveCommand;
 	inputParamsIocRegister params;
 	std::shared_ptr<MoveCommandMock> pa_ptr = std::make_shared<MoveCommandMock>(moveCommand);
-	ICommand_Ptr params_ptr = std::make_shared<MoveCommandMock>(moveCommand);
+	ICommand_Ptr params_ptr = pa_ptr;
 	params.strCommand = "MoveCommandMock";
 	params.command = params_ptr;
 	ioc.Resolve<ICommand>("IoC.Register", static_cast<void*>(&params));
 
-	//EXPECT_CALL(moveCommand, Execute()).WillOnce(testing::Return());
+	EXPECT_CALL(moveCommand, Execute()).WillOnce(testing::Return());
 
-	//ICommand_Ptr cur = ioc.Resolve<ICommand>("MoveCommandMock", static_cast<void*>(&params));
-	//cur->Execute();
+	ICommand_Ptr cur = ioc.Resolve<ICommand>("MoveCommandMock", static_cast<void*>(&params));
+	cur->Execute();
+	
+
+
+
+	// ---------------------------------
+
+
+
+	//ioc.Resolve<ICommand>("MoveCommandMock", static_cast<void*>(&params)).Execute(); //тут пар-ов нет
+	//ioc.Resolve<ICommand>("IoC.Register", static_cast<void*>(&params)); // "завимисость", std::function void()
+
+
+
+
+
 }
