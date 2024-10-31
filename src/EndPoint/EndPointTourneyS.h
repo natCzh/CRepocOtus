7b83d7072916974abafa5cd010859d79018b333d
Natalia Zhukova <njukova@stc-spb.ru>
2024-10-31 17:30:33 +0300

    [+] добавлен EndPointTourneyS.h

---
+++ src/EndPoint/EndPointTourneyS.h
@@ -0,0 +1,55 @@
+#ifndef _END_POINT_H_
+#define _END_POINT_H_
+
+#include "EndPoint/IEndPoint.h"
+#include "EndPoint/InterpretCommand.h"
+#include "EndPoint/MessageAdapter.h"
+#include "EndPoint/Message.h" // TODO перепроверить подключения !поудалять !!!!!!!!!!
+#include "src/Common/ThreadDeque.h"
+#include "service/TourneyService/EventLoop.h"
+#include "Exception/MessageTourneySException.h"
+
+// EndPoint отвечает за прием сообщений извне
+
+// необходимо проинициализировать команды InterpretCommand
+
+// Класс то образом принимает запрос, конвертирует в json -> string -> IMessage -> InterpretCommand
+class EndPoint: public IEndPoint
+{
+public:
+	// TODO очередь нужно передавать тоже через IOC пока сделаем так через конструктор
+	EndPoint(ICommand_Ptr initCommand, std::shared_ptr<std::map<size_t, EventLoop> > collectionGame_)
+		: collectionGame(collectionGame_)
+	{
+		initCommand->Execute();
+	}
+	
+	void process()
+	{
+		// получили запрос
+		//из json -> string
+		// string -> IMessage
+		// TODO пеоределать interpretCommand создается всегда при вызове 
+		Message m;
+
+		/////////////////////////////////////////////////////////////
+		std::shared_ptr<IMessagable> messagable = std::make_shared<MessageAdapter>(std::make_shared<Message>(m));
+		// найдем в коллекции игр нужную команду
+		auto curGame = std::find(collectionGame->begin(), collectionGame->end(), messagable->getIdGame()); // TODO !!!! потом тут сделать проверку и доставить игру не по ид, а по хэшу, сделать паттерн корелейшен ид
+		if (curGame == collectionGame->end())
+			throw MessageTourneySException("Message failed - id of game isn't correct");
+
+		std::shared_ptr<InterpretCommand> interpretCommand = std::make_shared<InterpretCommand>(std::make_shared<IMessagable>(messagable)); // это мы потом запихиваем в очередь eventloop
+		auto indexGame = curGame->second.findIndexCollectionGameId(messagable->getIdObject());
+
+
+		// те interpret Cоmmand мы положим в очередь игры уже для нужного объекта
+	}
+
+private:
+
+	std::shared_ptr<std::map<size_t, EventLoop> > collectionGame; // ссылка на очередь игр
+
+};
+
+#endif /* _END_POINT_H_ */
