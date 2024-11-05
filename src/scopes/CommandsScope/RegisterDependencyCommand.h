7cd104c8c1e79522f959cce1138c3aa3bf6bb55b
Natalia Zhukova <njukova@stc-spb.ru>
2024-11-05 15:45:03 +0300

    [+] RegisterDependencyCommand

---
+++ src/scopes/CommandsScope/RegisterDependencyCommand.h
@@ -0,0 +1,33 @@
+﻿#ifndef _REGISTER_DEPENDENCY_COMMAND_H_
+#define _REGISTER_DEPENDENCY_COMMAND_H_
+
+#include <functional>
+
+#include "Command/ICommand.h"
+#include "scopes/DependencyResolve.h"
+
+template<typename... Args>
+struct tuple;
+
+template<typename T, typename... Args>
+class RegisterDependencyCommand: public ICommand
+{
+	RegisterDependencyCommand(std::shared_ptr<Scopes::DependencyResolve> depency_, std::string key_, std::function<T(Args... arg)> fun_)
+		: depency(depency_)
+		, key(key_)
+		, fun(fun_)
+	{}
+
+	void Execute()
+	{
+		auto currentScope = depency->GetCurrentScope();
+		currentScope->Add(key, boost::any_cast(fun));
+	}
+
+protected:
+	std::shared_ptr<Scopes::DependencyResolve>														depency;
+	std::string																						key;
+	std::function<T(Args... arg)>																	fun;
+};
+
+#endif /* _REGISTER_DEPENDENCY_COMMAND_H_ */
