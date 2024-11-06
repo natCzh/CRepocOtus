6ba30ad893eba2baeaec8c03bb70d5765b97fcf6
Natalia Zhukova <njukova@stc-spb.ru>
2024-11-06 16:30:15 +0300

    [+] fixed double shared_ptr

--- src/scopes/CommandsScope/RegisterDependencyCommand.h
+++ src/scopes/CommandsScope/RegisterDependencyCommand.h
@@ -14,10 +14,13 @@ template<typename T, typename... Args>
 class RegisterDependencyCommand: public ICommand
 {
 public:
-	~RegisterDependencyCommand() {}
+	~RegisterDependencyCommand()
+	{
+		
+	}
 
-	template<typename T, typename... Args>
-	RegisterDependencyCommand(Scopes::DependencyResolve* depency_, std::string key_, std::function<T(Args... args)> fun_, Args... args)
+	template<typename T, typename... Args, typename... Args1>
+	RegisterDependencyCommand(Scopes::DependencyResolve* depency_, std::string key_, std::function<T(Args... args)> fun_, Args1... args)
 		: depency(depency_)
 		, key(key_)
 		, fun(fun_)
@@ -30,7 +33,7 @@ public:
 		currentScope->Add(key, v);
 
 		auto d = currentScope->GetValueOrDefault("A");
-		std::function<T()> df = boost::any_cast<std::function<T()>>(d);
+		std::function<T()> df = boost::any_cast<std::function<T()> >(d);
 		ICommand_Ptr c = df();
 		c->Execute();
 
@@ -44,9 +47,9 @@ public:
 	}
 
 protected:
-	std::shared_ptr<Scopes::DependencyResolve>														depency;
+	Scopes::DependencyResolve*																		depency;
 	std::string																						key;
-	std::function<T(Args... args)>																				fun;
+	std::function<T(Args... args)>																	fun;
 };
 
 #endif /* _REGISTER_DEPENDENCY_COMMAND_H_ */
