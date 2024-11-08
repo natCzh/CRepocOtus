94d1911dc51b031aa9ab1b08a44e6cf4f41caaf6
Natalia Zhukova <njukova@stc-spb.ru>
2024-11-08 14:51:17 +0300

    [*] Изменено хранение velocity в spaceship. теперь это как модуль

--- src/Common/SpaceShip.h
+++ src/Common/SpaceShip.h
@@ -17,8 +17,7 @@ public:
 	{
 		mapKey["position_x"] = 0;
 		mapKey["position_y"] = 0;
-		mapKey["velocity_x"] = 0;
-		mapKey["velocity_y"] = 0;
+		mapKey["velocity"] = 0;
 		mapKey["direction"] = 0;
 		mapKey["directionNumber"] = 4; // можно двигаться только в 4 стороны 
 		mapKey["shotRange"] = 0;
