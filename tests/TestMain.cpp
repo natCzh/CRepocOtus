6767971eac3f9f18205e9e5416ed28815231ba31
Natalia Zhukova <njukova@stc-spb.ru>
2024-11-14 17:19:25 +0300

    [+] added test for CorePluginBattleCommand

--- tests/TestMain.cpp
+++ tests/TestMain.cpp
@@ -3,27 +3,31 @@
 # include "gmock/gmock.h"
 
 //#include "src/IoC.h"
-#include "src/Exception/IoCException.h"
-#include "src/Command/MoveCommand.h"
+//#include "src/Exception/IoCException.h"
+//#include "src/Command/MoveCommand.h"
+#include "service/TourneyService/CorePluginBattleCommand.h"
 
 
-TEST(TestIoC, test_scope)
+TEST(Test1, test_scope)
 {
-	/*MoveCommand curMove;
-	ICommand_Ptr ICurrentCommand = std::make_shared<MoveCommand>(curMove);
-	inputParamsIocRegister params;
-	params.strCommand = "MoveCommand";
-	params.command = ICurrentCommand;
+    CorePluginBattleCommand core;
 
-	IoC ioc;
-	// скоупа нету, поэтому эксепшен
 
-	std::string key = "IoC.Register";
-	EXPECT_THROW(ioc.Resolve<ICommand>(key, (void*)(&params)), IoCException);
+    /*MoveCommand curMove;
+    ICommand_Ptr ICurrentCommand = std::make_shared<MoveCommsand>(curMove);
+    inputParamsIocRegister params;
+    params.strCommand = "MoveCommand";
+    params.command = ICurrentCommand;
 
-	// добавляем новый скоуп
-	size_t idScope = 0;
-	EXPECT_NO_THROW(ioc.Resolve<ICommand>("Scopes.New", static_cast<void*>(&idScope)));
-	EXPECT_NO_THROW(ioc.Resolve<ICommand>("Scopes.Current", static_cast<void*>(&idScope)));*/
+    IoC ioc;
+    // скоупа нету, поэтому эксепшен
+
+    std::string key = "IoC.Register";
+    EXPECT_THROW(ioc.Resolve<ICommand>(key, (void*)(&params)), IoCException);
+
+    // добавляем новый скоуп
+    size_t idScope = 0;
+    EXPECT_NO_THROW(ioc.Resolve<ICommand>("Scopes.New", static_cast<void*>(&idScope)));
+    EXPECT_NO_THROW(ioc.Resolve<ICommand>("Scopes.Current", static_cast<void*>(&idScope)));*/
 
 }
