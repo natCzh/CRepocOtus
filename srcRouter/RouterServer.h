c02c3ad418bee200f08312eda506fdf848c35c5e
Natalia Zhukova <njukova@stc-spb.ru>
2024-11-28 09:03:56 +0300

    [*] перенос файлов в router

---
+++ srcRouter/RouterServer.h
@@ -0,0 +1,17 @@
+﻿#ifndef _ROUTER_SERVER_H_
+#define _ROUTER_SERVER_H_
+
+// главная класс роутера как соединение со всеми сервисами
+class RouterServer
+{
+public:
+
+    RouterServer(){}
+
+    virtual ~RouterServer(){}
+\
+private:
+
+};
+
+#endif /* _ROUTER_SERVER_H_ */
